1951691760801d5a43dc7b3f556e3feb97d46394
Nicholas Ren <nicholas85211@gmail.com>
2013-04-10 13:45:50 +0800

    change script of creating scala project scaffold

--- scripts/project_scaffold.rb
+++ scripts/project_scaffold.rb
@@ -1,34 +1,59 @@
 #!/usr/bin/env ruby
 require "fileutils"
 
-def build_file_content
+def build_file_content prj_name
   <<-EOS
-    version := "1.0.0"
+    name := \"#{prj_name}\"
 
-    scalaVersion := "2.9.2"
+    version := \"1.0.0\"
 
-    libraryDependencies += "org.scalatest" %% "scalatest" % "1.8" % "test"
+    scalaVersion := \"2.9.2\"
 
-    libraryDependencies += "junit" % "junit" % "4.10" % "test"
+    libraryDependencies += \"org.scalatest\" %% \"scalatest\" % \"1.8\" % \"test\"
+
+    libraryDependencies += \"junit" % "junit\" % \"4.10\" % \"test\"
   EOS
 end
 
-def create_scaffold prj_name
-  FileUtils.mkdir_p prj_name
-  FileUtils.cd prj_name do
+
+def plugin_file_content
+  "addSbtPlugin(\"com.typesafe.sbteclipse\" % \"sbteclipse-plugin\" % \"2.1.2\")"
+end
+
+
+def create_build_file prj_name
+  File.open('build.sbt', 'w') do |f|
+    f.puts build_file_content(prj_name)
+  end
+end
+
+def create_src_folds
     [
       "src/main/scala",
       "src/main/java",
       "src/main/resources",
       "src/test/scala",
       "src/test/java",
-      "src/test/resources"
+      "src/test/resources",
+      "project"
     ].each do |dir|
       FileUtils.mkdir_p dir
     end
+end
 
-    FileUtils.touch "build.sbt"
-    system "echo \"#{build_file_content}\" >> build.sbt"
+
+def create_plugin_file
+  File.open('project/plugins.sbt', 'w') do |f|
+    f.puts plugin_file_content
+  end
+end
+
+def create_scaffold prj_name
+  FileUtils.mkdir_p prj_name
+  FileUtils.cd prj_name do
+    create_src_folds
+    create_build_file prj_name
+    create_plugin_file
   end
 end
 
@@ -40,5 +65,3 @@ else
   create_scaffold prj_name
   puts "creating project #{prj_name} succeed!"
 end
-
-
