7b2ca77ad4af4cd50551d6bbc03d66860b1e84ef
Nicholas Ren <nicholas85211@gmail.com>
2013-07-03 13:37:02 +0800

    refine scala project

--- scripts/project_scaffold.rb
+++ scripts/project_scaffold.rb
@@ -5,21 +5,13 @@ def build_file_content prj_name
   <<-EOS
 name := \"#{prj_name}\"
 
-version := \"1.0.0\"
+version := \"0.1\"
 
-scalaVersion := \"2.9.2\"
+scalaVersion := \"2.10.1\"
 
-libraryDependencies += \"org.scalatest\" %% \"scalatest\" % \"1.8\" % \"test\"
-
-libraryDependencies += \"junit" % "junit\" % \"4.10\" % \"test\"
+libraryDependencies += \"org.scalatest\" % \"scalatest_2.10\" % \"1.9.1\" % \"test\"
   EOS
 end
-
-
-def plugin_file_content
-  "addSbtPlugin(\"com.typesafe.sbteclipse\" % \"sbteclipse-plugin\" % \"2.1.2\")"
-end
-
 
 def create_build_file prj_name
   File.open('build.sbt', 'w') do |f|
@@ -30,22 +22,12 @@ end
 def create_src_folds
     [
       "src/main/scala",
-      "src/main/java",
       "src/main/resources",
       "src/test/scala",
-      "src/test/java",
       "src/test/resources",
-      "project"
     ].each do |dir|
       FileUtils.mkdir_p dir
     end
-end
-
-
-def create_plugin_file
-  File.open('project/plugins.sbt', 'w') do |f|
-    f.puts plugin_file_content
-  end
 end
 
 def create_scaffold prj_name
@@ -53,7 +35,6 @@ def create_scaffold prj_name
   FileUtils.cd prj_name do
     create_src_folds
     create_build_file prj_name
-    create_plugin_file
   end
 end
 
